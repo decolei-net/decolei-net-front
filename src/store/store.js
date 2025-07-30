82d0f7339ce1ecc001478cb40ce96575dd77ac72
Leônidas Netto <leonidasdcn@gmail.com>
2025-07-30 17:21:39 -0300

    refactor: identação

--- src/store/store.js
+++ src/store/store.js
@@ -1,53 +1,45 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit"; 
-// import thunk from 'redux-thunk'; // <--- 1. REMOVA ESTA LINHA
-import authReducer from './authSlice.js'; // Adicionei .js por segurança
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import authReducer from './authSlice.js';
 
 const loadState = () => {
-    try{
-        const serializedState = localStorage.getItem('app_state');
-        
-        if(serializedState === null){
-            return undefined;
-        }
+  try {
+    const serializedState = localStorage.getItem('app_state');
 
-        return JSON.parse(serializedState)
-    }catch(err){
-        return undefined;
-    };
-}
-
-
-const saveState = (state) => {
-    try {
-        // AVISO: Havia um ".at" sobrando aqui, eu corrigi.
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem('app_state', serializedState);
-        
-    } catch (err) {
-        console.error("Erro ao salvar o state  da aplicação: ", err);
-        
+    if (serializedState === null) {
+      return undefined;
     }
+
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
 };
 
+const saveState = (state) => {
+  try {
+    // AVISO: Havia um ".at" sobrando aqui, eu corrigi.
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('app_state', serializedState);
+  } catch (err) {
+    console.error('Erro ao salvar o state  da aplicação: ', err);
+  }
+};
 
 const rootReducer = combineReducers({
-    auth: authReducer,
+  auth: authReducer,
 });
-
 
 const preloadedState = loadState();
 
 const store = configureStore({
-    reducer: rootReducer,
-    // middleware: [thunk], // <--- 2. REMOVA ESTA LINHA TAMBÉM
-    preloadedState
+  reducer: rootReducer,
+  // middleware: [thunk], // <--- 2. REMOVA ESTA LINHA TAMBÉM
+  preloadedState,
 });
 
-store.subscribe(
-    () => {
-        saveState({auth: store.getState().auth})
-    }
-);
+store.subscribe(() => {
+  saveState({ auth: store.getState().auth });
+});
 
 // Adicione esta linha para exportar a store
 export default store;
