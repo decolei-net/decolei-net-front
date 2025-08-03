a5941410b5f744bbe6ffa22268594d5b69033ac7
Leonardo Amyntas Filho <l.de.freitas.filho@AVAPC-029750502>
2025-08-03 18:22:47 -0300

    delete folder

--- src/layouts/ClienteLayout.jsx
+++ src/layouts/ClienteLayout.jsx
@@ -1,10 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar.jsx';
-import Footer from '../components/Footer.jsx'; // <-- PASSO 1: Importe o Footer
+import Footer from '../components/Footer.jsx'; 
 
 export default function ClienteLayout() {
   return (
-    // <-- PASSO 2: Adicione as classes na div principal
     <div className="flex flex-col min-h-screen">
       <NavBar />
       {/* 
@@ -12,11 +11,10 @@ export default function ClienteLayout() {
         - Removido 'container mx-auto p-6' daqui, pois cada página pode ter seu próprio container.
         - Adicionado 'flex-grow' para empurrar o rodapé para baixo.
       */}
-      <main className="flex-grow"> {/* <-- PASSO 3: Adicione a classe aqui */}
+      <main className="flex-grow"> 
         <Outlet />
       </main>
       
-      {/* <-- PASSO 4: Adicione o componente Footer aqui */}
       <Footer />
     </div>
   );
